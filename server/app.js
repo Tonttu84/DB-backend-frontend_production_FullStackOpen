f1540fb10940b6c496951b577cf8b926add4b44f
Johannes <you@example.com>
2026-04-12 11:18:41 +0300

    fixing the main page

--- server/app.js
+++ server/app.js
@@ -13,6 +13,13 @@ app.use(express.static(path.resolve('dist')))
 
 app.use('/api/persons', personsRouter)
 
+app.use((req, res, next) => {
+	if (req.originalUrl.startsWith('/api')) {
+	  return next()
+	}
+	res.sendFile(path.resolve('dist/index.html'))
+  })
+
  app.use((req, res) => {
    res.status(404).json({ error: 'unknown endpoint' })
  })
